f7be5cc2392194be1f7af12db40add9931840d5a
apnasaurav <66203296+apnasaurav@users.noreply.github.com>
2020-06-30 21:58:45 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 var welcome_message
 alert("For Query contact 8544104446!");
+(function(exports) {
+  "use strict";
+
+  function initMap() {
+    exports.map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      zoom: 8
+    });
+  }
+
+  exports.initMap = initMap;
+})((this.window = this.window || {}));
